1daba6a7c42d49f0bcb3beac807c11676be1c570
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2022-10-08 22:09:12 -0600

    moar greedy mingle algorithm

--- greedy.go
+++ greedy.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 func Greedy(person Person, maxSize int) []Mingle {
 	var mingles []Mingle
 
@@ -70,6 +72,48 @@ func GreedyPeople(people []*Person, maxSize int) []Mingle {
 			res = append(res, k)
 		}
 		mingles = append(mingles, Mingle{res, maxSize})
+	}
+
+	return mingles
+}
+
+func MoarGreedyPeople(people []*Person, maxSize int) []Mingle {
+	sort.Slice(people, func(i, j int) bool {
+		// sort by most preferences first.
+		return len(people[i].Prefs) >= len(people[j].Prefs)
+	})
+
+	var (
+		mingles []Mingle
+		matches = make(map[string]map[string]bool)
+		group   = make(map[string]struct{})
+	)
+
+	for _, person := range people {
+		p1 := person.Name
+		for _, p2 := range person.Prefs {
+			if matches[p1][p2] {
+				continue
+			}
+			if matches[p1] == nil {
+				matches[p1] = make(map[string]bool)
+			}
+			if matches[p2] == nil {
+				matches[p2] = make(map[string]bool)
+			}
+			matches[p1][p2] = true
+			matches[p2][p1] = true
+			group[p1] = struct{}{}
+			group[p2] = struct{}{}
+			if len(group) >= maxSize {
+				res := make([]string, 0, len(group))
+				for k := range group {
+					res = append(res, k)
+				}
+				mingles = append(mingles, Mingle{res, maxSize})
+				group = make(map[string]struct{})
+			}
+		}
 	}
 
 	return mingles
